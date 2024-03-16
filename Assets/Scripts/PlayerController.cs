bf38f41959252e74195b18ecfce3814377086f07
miqjection <erdemnaz01@gmail.com>
2024-03-17 02:52:24 +0300

    Karakter kontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    private bool isGrounded;
+
+    private float horizontalInput;
+    private float scaleX;
+
     private Rigidbody2D rb;
-    float horizontalInput;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        scaleX = transform.localScale.x;
     }
 
     void Update()
     {
-        Walk();
         WalkAnimation();
+        LookRotation();
+
+        print(isGrounded);
 
 
+        if (isGrounded)
+            Walk();
+
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            //isGrounded = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && rb.velocity.y > 0f)
+        {
+            //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
     }
 
     private void Walk()
@@ -26,8 +51,13 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(horizontalInput *100 * speed * Time.deltaTime, 0);
 
-        print(movement);
         rb.velocity = movement;
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        isGrounded = false;
     }
 
     private void WalkAnimation()
@@ -38,14 +68,37 @@ public class PlayerController : MonoBehaviour
         }
 
         else
+        {
             PlayerAnimation.instance.isWalk = false;
+        }
     }
 
     private void LookRotation()
     {
         if(horizontalInput < 0)
         {
-            
+            transform.localScale = new Vector2 (-scaleX, transform.localScale.y);
+        }
+
+        else if(horizontalInput > 0) 
+        {
+            transform.localScale = new Vector2(scaleX, transform.localScale.y); 
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
         }
     }
 }
