75fcad06f3a25552f6102594c947f90789e3d59f
ceylincinuk <ceycinuk@gmail.com>
2024-03-17 17:52:40 +0300

    sahnegecisi

--- Assets/Scripts/Ceylin/NextScene.cs
+++ Assets/Scripts/Ceylin/NextScene.cs
@@ -16,7 +16,7 @@ public class NextScene : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
 
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.tag=="Untagged")
         {
             SceneManager.LoadScene(SceneIndex);
         }
