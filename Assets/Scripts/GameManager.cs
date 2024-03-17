d370609dac4b18077cfd8a3b5f72a836b9328e83
miqjection <erdemnaz01@gmail.com>
2024-03-17 14:01:02 +0300

    tekerlekanim

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject SeeEnemyTrigger;
+    [SerializeField] private GameObject girlWithCatCar;
+    [SerializeField] private GameObject girl;
     void Start()
     {
         Invoke("EnemyTriggerSetActiveTrue", 10);
@@ -14,11 +16,13 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void EnemyTriggerSetActiveTrue()
     {
         SeeEnemyTrigger.SetActive(true);
+        girl.SetActive(false);
+        girlWithCatCar.SetActive(true);
     }
 }
